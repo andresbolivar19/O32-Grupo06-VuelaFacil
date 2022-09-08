12f31bb256d789c1078e4da720b1ad600b147b30
Andres Bolivar <andresbolivar19@gmail.com>
2022-09-07 21:18:42 -0500

    Se agrega backend descomprimido

---
+++ backend032/src/main/java/com/misiontic/backend032/Backend032Application.java
@@ -0,0 +1,13 @@
+package com.misiontic.backend032;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Backend032Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Backend032Application.class, args);
+	}
+
+}
